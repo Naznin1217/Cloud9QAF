c656d9b79b4a968f84abd6a1bca8908c750c4d9d
nazninakhter <nazninakhter@10.67.228.243>
2021-10-17 17:10:16 -0400

    second commit

---
+++ src/test/java/common/GoogleBase.java
@@ -0,0 +1,71 @@
+package common;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleBase {
+	
+	public WebDriver driver;
+	
+	String browser = "chrome";
+	
+	
+	public void launchBrowser() {
+		
+		if(browser=="chrome") {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();		
+		
+		}
+		
+		else if (browser=="firefox") {
+			
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			
+			
+		}
+		else if (browser=="safari") {
+			
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver();
+		}
+		
+		else {
+			
+			System.out.println("Wrong Browser");
+		}
+		
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.get("http://www.google.com");
+		
+		
+		
+	}
+
+	public void closeBrowser() {
+		
+		driver.close();
+		
+	}
+	
+	
+	public void closeAllBrowser() {
+		
+		driver.quit();
+		
+		
+	}
+}
+
+
